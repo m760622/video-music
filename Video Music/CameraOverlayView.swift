812e489a1e05bb959c98a8bacd99838448ca6cd2
Will Murat Said <will.said7@gmail.com>
2018-03-10 22:48:25 -0500

    Everything is ready as far as code goes. Now just need to put on app store.

--- Video Music/CameraOverlayView.swift
+++ Video Music/CameraOverlayView.swift
@@ -20,28 +20,7 @@ class CameraOverlayView: UIView {
 
 
     @IBAction func tappedButton(_ sender: UIButton) {
-//        print("x")
-//        print(AVAudioSession.sharedInstance().isOtherAudioPlaying)
-//        print("hellow world i tapped button")
-//        // now need to alert view controller to change image picker to front or rear
-////       fixAudio()
-////       delegate.didSwitch(overlayView: self)
-//        print("ew")
-//        print(AVAudioSession.sharedInstance().isOtherAudioPlaying)
-////        fixAudio()
-//        print(AVAudioSession.sharedInstance().isOtherAudioPlaying)
     }
-    
-//    func fixAudio() {
-//        do {
-//            _ = try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord, with: .mixWithOthers)
-//            try AVAudioSession.sharedInstance().setMode(AVAudioSessionModeDefault)
-//            print(AVAudioSession.sharedInstance().mode)
-//        } catch let error as NSError {
-//            print(error)
-//        }
-//    }
-    
-    
+
 
 }
--- Video Music/ViewController.swift
+++ Video Music/ViewController.swift
@@ -5,6 +5,8 @@
 //  Created by Will Said on 2/26/18.
 //  Copyright © 2018 Will Said. All rights reserved.
 //
+// Todo: enable editing, resume music on pause, and fix dual-camera switch problem
+//
 
 import UIKit
 import AVFoundation
@@ -18,60 +20,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     
     var imagePicker: UIImagePickerController! = UIImagePickerController()
-    
     let saveFileName = UUID().uuidString
     
-    func fixAudio() {
-        do {
-            _ = try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord, with: .mixWithOthers)
-            try AVAudioSession.sharedInstance().setMode(AVAudioSessionModeDefault)
-            print(AVAudioSession.sharedInstance().mode)
-        } catch let error as NSError {
-            print(error)
-        }
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+
+        let screensize: CGRect = UIScreen.main.bounds
+        let screenWidth = screensize.width
+        let screenHeight = screensize.height
+        scrollView.contentSize = CGSize(width: screenWidth, height: screenHeight)
     }
     
-//    func didSwitch(overlayView:CameraOverlayView) {
-//        print("u")
-//        print(AVAudioSession.sharedInstance().isOtherAudioPlaying)
-////        try? AVAudioSession.sharedInstance().setMode(AVAudioSessionModeDefault)
-////        fixAudio()
-//        print("ut")
-//        print(AVAudioSession.sharedInstance().isOtherAudioPlaying)
-//        if imagePicker.cameraDevice == .front {
-//            print(AVAudioSession.sharedInstance().isOtherAudioPlaying)
-//            imagePicker.cameraDevice = .rear
-//        } else {
-//            print(AVAudioSession.sharedInstance().isOtherAudioPlaying)
-//            imagePicker.cameraDevice = .front
-//        }
-//        print(AVAudioSession.sharedInstance().isOtherAudioPlaying)
-//
-//        print("fin")
-////        fixAudio()
-//
-//
-//    }
 
     
+    
+    
+    
     @IBAction func buttonClicked(_ sender: Any) {
-        
         takeVideo(front: false)
-        
         fixAudio()
-        
     }
     
     @IBAction func frontButtonClicked(_ sender: UIButton) {
         takeVideo(front: true)
         fixAudio()
     }
-    
-    @IBOutlet weak var cameraButton: UIButton!
-    
-    
-    @IBOutlet var camPreview: UIView!
-
     
     
     
@@ -90,29 +69,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 imagePicker.cameraDevice = .rear
             }
             
-            //customView stuff
-//            let overlayViewController = CameraOverlayViewController(
-//                nibName:"CameraOverlayViewController",
-//                bundle: nil
-//            )
-//            let cameraView:CameraOverlayView = overlayViewController.view as! CameraOverlayView
-//
-//            cameraView.frame = imagePicker.view.frame
+            fixAudio()
             
-            
-//            cameraView.delegate = self
-            
-//            imagePicker.cameraOverlayView = cameraView
+            present(imagePicker, animated: true, completion: nil)
             
             fixAudio()
             
-            present(imagePicker, animated: true, completion: {
-//                self.imagePicker.cameraOverlayView = cameraView
-            })
-            
-            fixAudio()
-            
-        }  else { //no camera found -- alert the user.
+        }  else {
+            //no camera found -- alert the user.
             let alertVC = UIAlertController(
                 title: "No Camera",
                 message: "Sorry, this device has no camera",
@@ -129,23 +93,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        imagePicker.delegate = self
-    }
+    
+    
+    
+    
+    
     
     
     
     // Finished recording a video
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
-        imagePicker.dismiss(animated: true, completion: {
-        })
+        imagePicker.dismiss(animated: true, completion: nil)
         
         if let pickedVideo:URL = info[UIImagePickerControllerMediaURL] as? URL {
             
-
-
             // Save video to the main photo album
             UISaveVideoAtPathToSavedPhotosAlbum(pickedVideo.relativePath, self, nil, nil)
 
@@ -160,6 +122,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 try videoData?.write(to: dataPath, options: [])
             } catch let error as NSError {
                 print(error)
+                failureAlert()
+                return
             }
 
             print("Saved to " + dataPath.absoluteString)
@@ -177,82 +141,42 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 alertVC,
                 animated: true,
                 completion: nil)
-        }
-
-        if MPMusicPlayerController.systemMusicPlayer.playbackState == .playing {
-            print("Music is playing")
         } else {
-            print("Play your music")
-            //TODO: automatically resume background music when a user plays their own video
+            failureAlert()
         }
 
-        
+    }
+    
+    func failureAlert() {
+        let alertVC = UIAlertController(
+            title: "Failure",
+            message: "Sorry, video was not saved.",
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",
+            style:.default,
+            handler: nil)
+        alertVC.addAction(okAction)
+        present(
+            alertVC,
+            animated: true,
+            completion: nil)
     }
     
     
-    
-    
-    func imagePickerController2(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-
-        picker.dismiss(animated: true, completion: nil)
-        let manager = FileManager.default
-
-        guard let documentDirectory = try? manager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true) else {return}
-        guard let mediaType = info[UIImagePickerControllerMediaType] as? String else {return}
-        guard let url = info[UIImagePickerControllerMediaURL] as? NSURL else {return}
-
-        if mediaType == kUTTypeMovie as String || mediaType == kUTTypeVideo as String {
-            let asset = AVAsset(url: url as URL)
-            let length = Float(asset.duration.value) / Float(asset.duration.timescale)
-            print("video length: \(length) seconds")
-
-            let start = info["_UIImagePickerControllerVideoEditingStart"] as? Float
-            let end = info["_UIImagePickerControllerVideoEditingEnd"] as? Float
-
-
-//            var outputURL = documentDirectory.URLByAppendingPathComponent("output")
-            var outputURL = documentDirectory.appendingPathComponent("output")
-
-            do {
-                try manager.createDirectory(at: outputURL, withIntermediateDirectories: true, attributes: nil)
-//                outputURL = outputURL.URLByAppendingPathComponent("output.mp4")
-                outputURL = documentDirectory.appendingPathComponent("output.mp4")
-            }catch let error {
-                print(error)
-            }
-
-            //Remove existing file
-            _ = try? manager.removeItem(at: outputURL)
-
-
-            guard let exportSession = AVAssetExportSession(asset: asset, presetName: AVAssetExportPresetHighestQuality) else {return}
-            exportSession.outputURL = outputURL
-            exportSession.outputFileType = AVFileType.mp4
-
-            let startTime = CMTime(seconds: Double(start ?? 0), preferredTimescale: 1000)
-            let endTime = CMTime(seconds: Double(end ?? length), preferredTimescale: 1000)
-            let timeRange = CMTimeRange(start: startTime, end: endTime)
-
-            exportSession.timeRange = timeRange
-            exportSession.exportAsynchronously{
-                switch exportSession.status {
-                case .completed:
-                    print("exported at \(outputURL)")
-
-                case .failed:
-                    print("failed \(exportSession.error)")
-
-                case .cancelled:
-                    print("cancelled \(exportSession.error)")
-
-                default: break
-                }
-            }
+        
+    func fixAudio() {
+        do {
+            _ = try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord, with: .mixWithOthers)
+            try AVAudioSession.sharedInstance().setMode(AVAudioSessionModeDefault)
+            print(AVAudioSession.sharedInstance().mode)
+        } catch let error as NSError {
+            print(error)
         }
     }
+
     
-        
-
-
+    
 }
+
 
